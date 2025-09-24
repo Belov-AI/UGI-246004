282c03ae603304a4fb1414c9e8cc7aaf2503061d
Belov-AI <Belov.Alexander@urfu.ru>
2025-09-24 19:53:34 +0500

    Задание 1 выполнено

---
+++ Task01/Task01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Кобаяси Иса");
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Хокку");
+
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Тихи, тихо ползи,");
+            Console.WriteLine("Улитка, по склону Фудзи.");
+            Console.WriteLine("Вверх, до самых высот.");
+
+            Console.ResetColor();
+        }
+    }
+}
