e77e853044060aad31870ab23ae8badb11860c14
Belov-AI <Belov.Alexander@urfu.ru>
2025-10-01 19:50:45 +0500

    Задание 3 выполнено

---
+++ Task03/Task03/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите двузначное число");
+
+            var n = int.Parse(Console.ReadLine());
+
+            var tenths = n / 10;
+            var units = n % 10;
+
+            var result = tenths * 1001 + units * 110;
+
+            Console.WriteLine("Вот что получилось: " + result);
+        }
+    }
+}
