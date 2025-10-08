c57de0da66030d20b8a634eb3e2334aef4b72933
anastasiika36-blip <anastasiika36@gmail.com>
2025-10-08 21:05:40 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,25 +4,12 @@ class Program
 {
     static void Main()
     {
-        try
-        {
-            Console.Write("Введите четырехзначное число: ");
-            int number = int.Parse(Console.ReadLine());
+        double x = F(2, 3) + F(5, 7) + F(4, 6);
+        Console.WriteLine($"x = {x:F3}");
+    }
 
-            if (number < 1000 || number > 9999)
-                throw new ArgumentException("Число должно быть четырехзначным!");
-
-            int result = (number / 1000) * 1000 +
-                        ((number / 10) % 10) * 100 +
-                        ((number / 100) % 10) * 10 +
-                        (number % 10);
-
-            Console.WriteLine($"Исходное число: {number}");
-            Console.WriteLine($"После перестановки: {result}");
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Ошибка: {ex.Message}");
-        }
+    static double F(double a, double b)
+    {
+        return (Math.Exp(a) - Math.Exp(-a)) / (Math.Exp(b) + Math.Exp(-b));
     }
 }
