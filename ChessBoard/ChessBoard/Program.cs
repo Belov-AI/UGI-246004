c6702133a04abd0876844bdc307c5859079bceeb
Belov-AI <Belov.Alexander@urfu.ru>
2025-10-29 20:50:13 +0500

    Chessboard программа закончена

--- ChessBoard/ChessBoard/Program.cs
+++ ChessBoard/ChessBoard/Program.cs
@@ -14,7 +14,34 @@ namespace Chessboard
             //что 1<= boardSize <= 26
             //и предложением повторного ввода, если условие не выполняется
             //Предусмотрите отказ от ввода (с последующим завершением программы).
+
+            while(true)
+            {
+                Console.WriteLine("Введите размер доски от 1 до 26 (Enter - выход из программы)");
+                var input = Console.ReadLine();
+
+                if (input == string.Empty)
+                    return;
+
+                if(!int.TryParse(input, out boardSize))
+                {
+                    Console.WriteLine("Ошибка ввода");
+                    continue;
+                }
+
+                if (boardSize > 26 || boardSize < 1)
+                {
+                    Console.WriteLine("Неверный размер");
+                    continue;
+                }
+
+                break;
+            }
+
             
+
+            
+
             PrintBoard(boardSize);           
 
             Console.ReadKey();
@@ -22,7 +49,42 @@ namespace Chessboard
 
         static void PrintBoard(int size)
         {
-            //написать метод печати шахматной доски размера (size x size)
+            var header = "  ";
+
+            for(int i = 0; i < size; i++)
+                header += (char)(0x61 + i);
+
+            Console.WriteLine(header);
+
+            var black = ConsoleColor.DarkBlue;
+            var white = ConsoleColor.Yellow;
+
+
+            for(int row = size; row > 0; row--)
+            {
+                Console.Write($"{row,2}");
+
+                ConsoleColor color;
+
+                if (row % 2 == 0)
+                    color = white;
+                else 
+                    color = black;
+
+                for(var i = 0; i < size; i++)
+                {
+                    PrintSquare(color);
+
+                    if (color == black)
+                        color = white;
+                    else
+                        color = black;
+                }
+
+                Console.WriteLine(row);
+            }
+                
+            Console.WriteLine(header);
         }
 
         /// <summary>
