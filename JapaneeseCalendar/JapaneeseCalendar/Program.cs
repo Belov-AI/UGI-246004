170312fddea2d3d58eae2a074e605e7db1eb96bc
Belov-AI <Belov.Alexander@urfu.ru>
2025-11-12 19:07:21 +0500

    Японский календарь на основе массивов

--- JapaneeseCalendar/JapaneeseCalendar/Program.cs
+++ JapaneeseCalendar/JapaneeseCalendar/Program.cs
@@ -19,50 +19,16 @@ namespace JapaneeseCalendar
             if (yearInCycle < 0)
                 yearInCycle += 60;
 
-            var animalNumber = yearInCycle % 12;
-            var colorNumber = yearInCycle / 12;
+            var animalIndex = yearInCycle % 12;
+            var colorIndex = yearInCycle / 12;
 
-            string color;
+            var colors = new[] { "зелен", "красн", "желт", "бел", "черн" };
 
-            if (colorNumber == 0)
-                color = "зелен";
-            else if (colorNumber == 1)
-                color = "красн";
-            else if (colorNumber == 2)
-                color = "желт";
-            else if (colorNumber == 3)
-                color = "бел";
-            else
-                color = "черн";
+            var animals = new[] { "ой крысы", "ой коровы", "ого тигра",
+                "ого зайца", "ого дракона", "ой змеи", "ой лошади", "ой овцы",
+                "ой обезьяны", "ой курицы", "ой собаки", "ой свиньи"};
 
-            string animal;
-
-            if (animalNumber == 0)
-                animal = "ой крысы";
-            else if (animalNumber == 1)
-                animal = "ой коровы";
-            else if (animalNumber == 2)
-                animal = "ого тигра";
-            else if (animalNumber == 3)
-                animal = "ого зайца";
-            else if (animalNumber == 4)
-                animal = "ого дракона";
-            else if (animalNumber == 5)
-                animal = "ой змеи";
-            else if (animalNumber == 6)
-                animal = "ой лошади";
-            else if (animalNumber == 7)
-                animal = "ой овцы";
-            else if (animalNumber == 8)
-                animal = "ой обезьяны";
-            else if (animalNumber == 9)
-                animal = "ой курицы";
-            else if (animalNumber == 10)
-                animal = "ой собаки";
-            else
-                animal = "ой свиньи";
-            
-            Console.WriteLine($"{year} г. — год {color}{animal}");
+            Console.WriteLine($"{year} г. — год {colors[colorIndex]}{animals[animalIndex]}");
         }
     }
 }
