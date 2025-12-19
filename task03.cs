150141afe3baf0867608bb400951a1988802927a
weirdlame <13serdem13@gmail.com>
2025-12-19 18:14:36 +0500

    Task 03 solution

---
+++ task03.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите пятизначное число");
+
+            var n = int.Parse(Console.ReadLine());
+
+            // Выделяем цифры пятизначного числа abcde
+            var a = n / 10000;       
+            var c = (n / 100) % 10;  
+            var e = n % 10;      
+
+            // Получаем трехзначное число ace (вычеркнули 2-ю и 4-ю цифры)
+            var result = a * 100 + c * 10 + e;
+
+            Console.WriteLine("Вот что получилось: " + result);
+        }
+    }
+}
