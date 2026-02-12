40df610efda3be7a6b2ba09465d63d96728e9f18
Belov-AI <Belov.Alexander@urfu.ru>
2026-02-12 13:24:10 +0500

    Созданы классы Point, Segment и Geometry

---
+++ Eucid/Eucid/Segment.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eucid
+{
+    public class Segment
+    {
+        public Point A;
+        public Point B;
+
+        public double Length
+        {
+            get
+            {
+                var dx = A.X - B.X;
+                var dy = A.Y - B.Y;
+
+                return Math.Sqrt(dx * dx + dy * dy);
+            }
+        }
+
+        public Segment(Point a, Point b)
+        {
+            A = a;
+            B = b;
+        }
+
+        public string GetInfo() => $"отрезок с концами в точках {A.GetInfo()} и {B.GetInfo()}";
+    }
+}
---
+++ Eucid/EuclidConsole/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Eucid;
+
+namespace EuclidConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Point(1, 2.5);
+            Console.WriteLine(p.GetInfo());
+
+            var s = new Segment(new Point(1, 1), new Point(-1, 3));
+            Console.WriteLine(s.GetInfo());
+            Console.WriteLine($"длина отрезка {s.Length:F3}");
+
+            s = Geometry.CreateSegment(new Point(-2, 1.5), new Point(0, 0));
+            Console.WriteLine(s.GetInfo());
+
+            //s = Geometry.CreateSegment(new Point(1, 1), new Point(1, 1));
+            //Console.WriteLine(s.GetInfo());
+        }
+    }
+}
