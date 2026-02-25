e0d8bd4631afdeb20395277594b9c9efb095f3d5
Belov-AI <Belov.Alexander@urfu.ru>
2026-02-25 16:05:04 +0500

    Модульные тесты классов Point и Segment

---
+++ Eucid/Euclid.UnitTests/PointTests.cs
@@ -0,0 +1,37 @@
+﻿using Eucid;
+
+namespace Euclid.UnitTests
+{
+    [TestFixture]
+    public class PointTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void ConstructorTest()
+        {
+            //arrange and act
+            var p = new Point(1, 2);
+
+            //assert
+            Assert.That(p.X, Is.EqualTo(1));
+            Assert.That(p.Y, Is.EqualTo(2));    
+        }
+
+        [Test]
+        public void GetInfo()
+        {
+            //arrange
+            var p = new Point(1.5, -3.1);
+
+            //act
+            var info = p.GetInfo();
+
+            //assert
+            Assert.That(info, Is.EqualTo("(1,5; -3,1)"));
+        }
+    }
+}
---
+++ Eucid/Euclid.UnitTests/SegmentTests.cs
@@ -0,0 +1,20 @@
+﻿using Eucid;
+
+namespace Euclid.UnitTests
+{
+    [TestFixture]
+    public class SegmentTests
+    {
+        [Test]
+        public void ConstructorTest()
+        {
+            var a = new Point(1.5, 2.3);
+            var b = new Point(-1.7, 4.8);
+
+            var s = new Segment(a, b);
+
+            Assert.That(s.A, Is.SameAs(a));
+            Assert.That(s.B, Is.SameAs(b));
+        }
+    }
+}
