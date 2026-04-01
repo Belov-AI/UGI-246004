ed93b289812655b953200a011794759dcd5bc4dd
Belov-AI <Belov.Alexander@urfu.ru>
2026-04-01 15:31:54 +0500

    Итераторы - проект Sequence

---
+++ Sequences/Sequences/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+using System.Threading;
+
+namespace Sequences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sequence = new Sequence();
+
+            PrintSecuence(sequence.Fibonacci());
+
+            Console.WriteLine();
+
+            PrintSecuence(sequence.Factorial());
+
+            Console.WriteLine();
+            PrintSecuence(sequence.Collatz(9));
+
+            Console.WriteLine();
+            PrintSecuence(sequence.Collatz(27));
+
+        }
+
+        static void PrintSecuence(IEnumerable<BigInteger> s)
+        {
+
+            foreach (var elem in s)
+            {
+                Thread.Sleep(200);
+                Console.WriteLine(elem);
+
+                if (Console.KeyAvailable)
+                {
+                    var key = Console.ReadKey();
+
+                    if (key.Key == ConsoleKey.Enter)
+                        break;
+                }                  
+            }
+        }
+    }
+}
---
+++ Sequences/Sequences/Sequence.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+
+namespace Sequences
+{
+    internal class Sequence
+    {
+        public IEnumerable<BigInteger> Fibonacci()
+        {
+            BigInteger a = 1;
+            BigInteger b = 1;
+
+            BigInteger c;
+
+            yield return a;
+            yield return b;
+
+            while (true)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+                yield return c;
+            } 
+        }
+
+        public IEnumerable<BigInteger> Factorial()
+        {
+            BigInteger n = 0;
+            BigInteger f = 1;
+
+            do
+            {
+                yield return f;
+                f *= ++n;
+            } while(true);          
+        }
+
+        public IEnumerable<BigInteger> Collatz(BigInteger n)
+        {
+            yield return n;
+
+            while (true)
+            {
+               if (n % 2 == 0)
+                    n /= 2;
+                else
+                    n = 3 * n + 1;
+
+                yield return n;
+
+                if (n == 1) break;
+            } 
+            
+
+            
+        }
+    }
+}
