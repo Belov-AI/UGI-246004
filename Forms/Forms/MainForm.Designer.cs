085c644befae3b14d7fcb0469398a47437e467a1
Belov-AI <Belov.Alexander@urfu.ru>
2026-04-08 16:04:53 +0500

    Формы

---
+++ Forms/Forms/MainForm.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Forms
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFormButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFormButton
+            // 
+            this.openFormButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.openFormButton.Location = new System.Drawing.Point(29, 56);
+            this.openFormButton.Name = "openFormButton";
+            this.openFormButton.Size = new System.Drawing.Size(219, 105);
+            this.openFormButton.TabIndex = 0;
+            this.openFormButton.Text = "Открыть дополнительную форму";
+            this.openFormButton.UseVisualStyleBackColor = true;
+            this.openFormButton.Click += new System.EventHandler(this.openFormButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.openFormButton);
+            this.Name = "MainForm";
+            this.Text = "Главная формая";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openFormButton;
+    }
+}
+
---
+++ Forms/Forms/MainForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Forms
+{
+    public partial class MainForm : Form
+    {
+        SecondForm secondForm = null;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void openFormButton_Click(object sender, EventArgs e)
+        {    
+            if(secondForm == null)
+            {
+                secondForm = new SecondForm();
+                secondForm.Show();
+            }               
+
+        }
+    }
+}
