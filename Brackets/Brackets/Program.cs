e1a8782df6bbe6ceb6e639244af2030bc197ca08
Belov-AI <Belov.Alexander@urfu.ru>
2026-05-06 13:52:03 +0500

    Стеки и очереди

---
+++ Brackets/Brackets/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brackets
+{
+    internal class Program
+    {
+        static Dictionary<char,char> brackets;
+        static void Main(string[] args)
+        {
+            brackets = new Dictionary<char, char>();
+            brackets[')'] = '(';
+            brackets[']'] = '[';
+            brackets['}'] = '{';
+            brackets['>'] = '<';
+            brackets['»'] = '«';
+
+            while (true)
+            {
+                Console.WriteLine("Введите выражение со скобками (ENTER - выход)");
+
+                var expression = Console.ReadLine();
+
+                if (expression == string.Empty) return;
+
+                if (AreBracketsCorrect(expression))
+                    Console.WriteLine("Скобки расставлены верно");
+                else
+                    Console.WriteLine("Скобки расставлены неверно");
+
+                Console.WriteLine();
+            }
+        }
+
+        static bool AreBracketsCorrect(string expr)
+        {
+            var stack = new Stack<char>();
+
+            foreach (char ch in expr) 
+                if(brackets.ContainsValue(ch))
+                    stack.Push(ch);
+                else if(brackets.ContainsKey(ch))
+                    if(stack.Count == 0 || stack.Pop() != brackets[ch])
+                        return false;
+
+            return stack.Count == 0;
+        }
+    }
+}
---
+++ Queues/Queues/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количесиво чисел");
+            var n = int.Parse(Console.ReadLine());
+
+            var queues = new Queue<int>[10];
+
+            for(int i = 0; i < queues.Length; i++)
+                queues[i] = new Queue<int>();
+
+            var rnd = new Random();
+
+            for(int counter = 0; counter < n; counter++)
+            {
+                var number = rnd.Next(1,1000);
+                var lastDigit = number % 10;
+                queues[lastDigit].Enqueue(number);
+
+                Console.Write($"{number} ");
+            }
+
+            Console.WriteLine("\n");
+
+            for (int i = 0; i < queues.Length; i++)
+            {
+                Console.WriteLine($"Числа, заканчивающиеся на {i}:");
+
+                while (queues[i].Count > 0)
+                    Console.Write($"{queues[i].Dequeue()} ");
+
+                Console.WriteLine("\n");
+            }
+
+        }
+    }
+}
