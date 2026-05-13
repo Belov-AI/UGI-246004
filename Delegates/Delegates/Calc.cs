def98278a3fd7cfc0f708f94ea1a551943b11b0e
Belov-AI <Belov.Alexander@urfu.ru>
2026-05-13 13:51:54 +0500

    Делегаты

---
+++ Delegates/Delegates/Calc.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    public class Calc
+    {
+        public double Cube(double x) => x * x * x;
+    }
+}
---
+++ Delegates/Delegates/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    public delegate double Function(double x);
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Function f;
+
+            double x = Math.PI / 2;
+
+            f = Math.Sin;
+            Console.WriteLine($"{ f(x):F3}");
+
+            f = Math.Cos;
+            Console.WriteLine($"{f(x):F3}");
+
+            f = Square;
+            Console.WriteLine($"{f(x):F3}");
+
+            var calc = new Calc();
+            f = calc.Cube;
+            Console.WriteLine($"{f(x):F3}");
+   
+        }
+
+        static double Square(double x) => x*x;
+        
+    }
+}
