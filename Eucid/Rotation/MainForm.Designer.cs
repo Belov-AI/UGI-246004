49a2ed45acce93f6b5981f86c4c89fd0948959ed
Belov-AI <Belov.Alexander@urfu.ru>
2026-05-21 22:15:09 +0500

    Начало разработки графического интерфейса

---
+++ Eucid/Rotation/MainForm.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace Rotation
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.centerLabel = new System.Windows.Forms.Label();
+            this.cXLabel = new System.Windows.Forms.Label();
+            this.cXTextBox = new System.Windows.Forms.TextBox();
+            this.cYLabel = new System.Windows.Forms.Label();
+            this.cYTextBox = new System.Windows.Forms.TextBox();
+            this.vLabel = new System.Windows.Forms.Label();
+            this.vXLlabel = new System.Windows.Forms.Label();
+            this.vXTtextBox = new System.Windows.Forms.TextBox();
+            this.vYLlabel = new System.Windows.Forms.Label();
+            this.vYTextBox = new System.Windows.Forms.TextBox();
+            this.vALabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // centerLabel
+            // 
+            this.centerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.centerLabel.Location = new System.Drawing.Point(12, 18);
+            this.centerLabel.Name = "centerLabel";
+            this.centerLabel.Size = new System.Drawing.Size(372, 34);
+            this.centerLabel.TabIndex = 0;
+            this.centerLabel.Text = "Координаты центра поворота";
+            // 
+            // cXLabel
+            // 
+            this.cXLabel.AutoSize = true;
+            this.cXLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cXLabel.Location = new System.Drawing.Point(69, 60);
+            this.cXLabel.Name = "cXLabel";
+            this.cXLabel.Size = new System.Drawing.Size(26, 25);
+            this.cXLabel.TabIndex = 1;
+            this.cXLabel.Text = "X";
+            // 
+            // cXTextBox
+            // 
+            this.cXTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cXTextBox.Location = new System.Drawing.Point(112, 55);
+            this.cXTextBox.Name = "cXTextBox";
+            this.cXTextBox.Size = new System.Drawing.Size(100, 30);
+            this.cXTextBox.TabIndex = 2;
+            // 
+            // cYLabel
+            // 
+            this.cYLabel.AutoSize = true;
+            this.cYLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cYLabel.Location = new System.Drawing.Point(69, 109);
+            this.cYLabel.Name = "cYLabel";
+            this.cYLabel.Size = new System.Drawing.Size(25, 25);
+            this.cYLabel.TabIndex = 1;
+            this.cYLabel.Text = "Y";
+            // 
+            // cYTextBox
+            // 
+            this.cYTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cYTextBox.Location = new System.Drawing.Point(112, 104);
+            this.cYTextBox.Name = "cYTextBox";
+            this.cYTextBox.Size = new System.Drawing.Size(100, 30);
+            this.cYTextBox.TabIndex = 2;
+            // 
+            // vLabel
+            // 
+            this.vLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.vLabel.Location = new System.Drawing.Point(12, 179);
+            this.vLabel.Name = "vLabel";
+            this.vLabel.Size = new System.Drawing.Size(372, 34);
+            this.vLabel.TabIndex = 0;
+            this.vLabel.Text = "Координаты центра поворота";
+            // 
+            // vXLlabel
+            // 
+            this.vXLlabel.AutoSize = true;
+            this.vXLlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.vXLlabel.Location = new System.Drawing.Point(69, 230);
+            this.vXLlabel.Name = "vXLlabel";
+            this.vXLlabel.Size = new System.Drawing.Size(26, 25);
+            this.vXLlabel.TabIndex = 1;
+            this.vXLlabel.Text = "X";
+            // 
+            // vXTtextBox
+            // 
+            this.vXTtextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.vXTtextBox.Location = new System.Drawing.Point(112, 225);
+            this.vXTtextBox.Name = "vXTtextBox";
+            this.vXTtextBox.Size = new System.Drawing.Size(99, 30);
+            this.vXTtextBox.TabIndex = 2;
+            // 
+            // vYLlabel
+            // 
+            this.vYLlabel.AutoSize = true;
+            this.vYLlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.vYLlabel.Location = new System.Drawing.Point(69, 279);
+            this.vYLlabel.Name = "vYLlabel";
+            this.vYLlabel.Size = new System.Drawing.Size(25, 25);
+            this.vYLlabel.TabIndex = 1;
+            this.vYLlabel.Text = "Y";
+            // 
+            // vYTextBox
+            // 
+            this.vYTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.vYTextBox.Location = new System.Drawing.Point(112, 274);
+            this.vYTextBox.Name = "vYTextBox";
+            this.vYTextBox.Size = new System.Drawing.Size(99, 30);
+            this.vYTextBox.TabIndex = 2;
+            // 
+            // vALabel
+            // 
+            this.vALabel.AutoSize = true;
+            this.vALabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.vALabel.Location = new System.Drawing.Point(12, 252);
+            this.vALabel.Name = "vALabel";
+            this.vALabel.Size = new System.Drawing.Size(34, 29);
+            this.vALabel.TabIndex = 3;
+            this.vALabel.Text = "A:";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.vALabel);
+            this.Controls.Add(this.vYTextBox);
+            this.Controls.Add(this.cYTextBox);
+            this.Controls.Add(this.vYLlabel);
+            this.Controls.Add(this.cYLabel);
+            this.Controls.Add(this.vXTtextBox);
+            this.Controls.Add(this.cXTextBox);
+            this.Controls.Add(this.vXLlabel);
+            this.Controls.Add(this.cXLabel);
+            this.Controls.Add(this.vLabel);
+            this.Controls.Add(this.centerLabel);
+            this.Name = "MainForm";
+            this.Text = "Поворот треугольника";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label centerLabel;
+        private System.Windows.Forms.Label cXLabel;
+        private System.Windows.Forms.TextBox cXTextBox;
+        private System.Windows.Forms.Label cYLabel;
+        private System.Windows.Forms.TextBox cYTextBox;
+        private System.Windows.Forms.Label vLabel;
+        private System.Windows.Forms.Label vXLlabel;
+        private System.Windows.Forms.TextBox vXTtextBox;
+        private System.Windows.Forms.Label vYLlabel;
+        private System.Windows.Forms.TextBox vYTextBox;
+        private System.Windows.Forms.Label vALabel;
+    }
+}
+
